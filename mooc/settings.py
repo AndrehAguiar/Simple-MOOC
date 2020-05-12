593000bdb2358f9318cc212f7f55f5a553aac14f
André Aguiar <andreaguiar.g@gmail.com>
2020-05-12 12:09:09 -0300

    Update settings.py

--- mooc/settings.py
+++ mooc/settings.py
@@ -136,6 +136,9 @@ LOGIN_REDIRECT_URL = 'core:home'
 LOGOUT_URL = 'accounts:logout'
 AUTH_USER_MODEL = 'accounts.User'
 
+import django_heroku
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 # Heroku settings
 import dj_database_url
 
@@ -145,6 +148,9 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
@@ -152,7 +158,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-import django_heroku
-# Activate Django-Heroku.
-django_heroku.settings(locals())
 
