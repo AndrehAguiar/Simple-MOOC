d3b085192981644b9e4bc7f2f377b896c6218a38
André Aguiar <andreaguiar.g@gmail.com>
2020-05-12 15:25:35 -0300

    Update settings.py

--- mooc/settings.py
+++ mooc/settings.py
@@ -161,7 +161,7 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'mooc/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
